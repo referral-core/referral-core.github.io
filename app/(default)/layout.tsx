daf2d9ba6427f7d996db1589dddefc5aa0f949e3
Stan Liu <stanleyliu@berkeley.edu>
2023-10-19 10:26:56 -0700

    feat: add google analytics to page

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react"
 import AOS from "aos"
 import "aos/dist/aos.css"
 
+import GoogleAnalytics from "@/components/google-analytics"
 import Header from "@/components/ui/header"
 import Footer from "@/components/ui/footer"
 
@@ -24,6 +25,7 @@ export default function DefaultLayout({
 
   return (
     <>
+      <GoogleAnalytics tagId="G-K3S7YRN738" />
       <Header />
       <main className="grow">{children}</main>
       <Footer />
