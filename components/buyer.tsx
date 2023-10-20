826f5088baf357bd5a881283b8addcffc7c3591e
Stan Liu <stanleyliu@berkeley.edu>
2023-10-20 16:28:00 -0700

    change color of feature blocks

--- components/buyer.tsx
+++ components/buyer.tsx
@@ -1,10 +1,10 @@
 import {Background} from "@/components/pricing";
-import Link from "next/link";
 import {
   ChatBubbleLeftRightIcon,
   CurrencyDollarIcon,
   LinkIcon
 } from "@heroicons/react/24/outline";
+import { Background as GradientBackground } from "@/components/cta"
 
 export default function Buyer() {
   return (
@@ -21,17 +21,17 @@ export default function Buyer() {
         </div>
 
         <div className="mx-auto grid max-w-xs gap-4 sm:max-w-none sm:grid-cols-2 md:max-w-xl lg:max-w-none lg:grid-cols-3 lg:gap-6" data-aos-id-featbl>
-          <div className="group flex flex-col rounded-2xl bg-gradient-to-tr from-[#0ba5e9] to-[#37ACDE] p-5 py-10 text-center text-white shadow-2xl" data-aos="fade-down" data-aos-anchor="[data-aos-id-featbl]">
+          <div className="group flex flex-col rounded-2xl bg-gradient-to-br from-slate-900 to-slate-700 p-5 py-10 text-center text-white shadow-2xl" data-aos="fade-down" data-aos-anchor="[data-aos-id-featbl]">
             <ChatBubbleLeftRightIcon className="mx-auto mb-2 h-12 w-12" />
             <div className="mb-2 mt-1 text-xl font-black tracking-tighter">High Quality Leads</div>
             <div className="text-lg opacity-80">Source leads that are <span className="font-extrabold">pre-qualified on intent and budget</span></div>
           </div>
-          <div className="group flex flex-col rounded-2xl bg-gradient-to-tr from-[#8b5cf6] to-[#936ceb] p-5 py-10 text-center text-white shadow-2xl" data-aos="fade-down" data-aos-anchor="[data-aos-id-featbl]" data-aos-delay="100">
+          <div className="group flex flex-col rounded-2xl bg-gradient-to-br from-slate-900 to-slate-700 p-5 py-10 text-center text-white shadow-2xl" data-aos="fade-down" data-aos-anchor="[data-aos-id-featbl]" data-aos-delay="100">
             <CurrencyDollarIcon className="mx-auto mb-2 h-12 w-12" />
             <div className="mb-2 mt-1 text-xl font-black tracking-tighter">Pricing</div>
             <div className="text-lg opacity-80">Only pay for leads that <span className="font-extrabold">convert to actual business</span> for you</div>
           </div>
-          <div className="group flex flex-col rounded-2xl bg-gradient-to-tr from-[#f471b6] to-[#f098c7] p-5 py-10 text-center text-white shadow-2xl" data-aos="fade-down" data-aos-anchor="[data-aos-id-featbl]" data-aos-delay="200">
+          <div className="group flex flex-col rounded-2xl bg-gradient-to-br from-slate-900 to-slate-700 p-5 py-10 text-center text-white shadow-2xl" data-aos="fade-down" data-aos-anchor="[data-aos-id-featbl]" data-aos-delay="200">
             <LinkIcon className="mx-auto mb-2 h-12 w-12" />
             <div className="mb-2 mt-1 text-xl font-black tracking-tighter">Network</div>
             <div className="text-lg opacity-80">Allow your network to pre-qualify leads for you so you can <span className="font-extrabold">focus on your clients</span></div>
