2abb62d00ff3a78b8d164f3ab38626ef9b093485
Stan Liu <stanleyliu@berkeley.edu>
2023-10-20 16:29:52 -0700

    fix build error

--- components/buyer.tsx
+++ components/buyer.tsx
@@ -4,7 +4,6 @@ import {
   CurrencyDollarIcon,
   LinkIcon,
 } from "@heroicons/react/24/outline"
-import { Background as GradientBackground } from "@/components/cta"
 
 export default function Buyer() {
   return (
